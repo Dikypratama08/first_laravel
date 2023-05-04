91b0d2ac9c6cec96601a0d7d24567fc483aa56de
Dikypratama08 <201220027@unmuhpnk.ac.id>
2023-05-04 15:00:15 +0700

    menambahkan function

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    public function getUser (User $user)
+    {
+        $dataUser = $user->get();
+        
+        return view('admin.user.viewUser', compact('dataUser'));
+    }
+
+    public function tambahForm()
+    {
+        return view('admin.user.tambahForm');
+    }
 }
